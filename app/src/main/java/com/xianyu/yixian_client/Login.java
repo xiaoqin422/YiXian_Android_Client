0ceb8a4b1f37c8b1ddd725e1b53278672cb66e26
mzh <mzh@DESKTOP-DVO9PA0>
2020-11-26 14:30:26 +0800

    初始框架，成功搭建了与C# 之间的TCP通道

--- app/src/main/java/com/xianyu/yixian_client/Login.java
+++ app/src/main/java/com/xianyu/yixian_client/Login.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.xianyu.yixian_client.Model.EchoClient;
+
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        EchoClient echoClient = new EchoClient("127.0.0.1",28015);
+        try {
+            echoClient.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
