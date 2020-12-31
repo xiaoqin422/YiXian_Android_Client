8f3fbb8c2c61bbcbfef6f4a8351477833cb5d0ea
a3123295667 <3123295667@qq.com>
2020-12-31 09:30:11 +0800

    ay的修改

---
+++ app/src/main/java/com/xianyu/yixian_client/BattleRepository/BattleRepositoryViewModel.java
@@ -0,0 +1,9 @@
+package com.xianyu.yixian_client.BattleRepository;
+
+import androidx.lifecycle.ViewModel;
+
+import com.xianyu.yixian_client.Model.Repository.RepositoryFactory;
+
+public class BattleRepositoryViewModel extends ViewModel {
+    public RepositoryFactory repositoryFactory;
+}
