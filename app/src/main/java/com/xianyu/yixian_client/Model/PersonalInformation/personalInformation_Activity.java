abbbed883150d5dc8649f42a68ed62293b5a59b1
86138 <1695450299@qq.com>
2020-12-31 09:33:37 +0800

    阿树的简单修改

---
+++ app/src/main/java/com/xianyu/yixian_client/Model/PersonalInformation/personalInformation_Activity.java
@@ -0,0 +1,16 @@
+package com.xianyu.yixian_client.Model.PersonalInformation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.xianyu.yixian_client.R;
+
+public class personalInformation_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_information_activity);
+    }
+}
