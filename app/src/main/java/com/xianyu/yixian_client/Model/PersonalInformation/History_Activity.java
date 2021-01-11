bdda7e0e7e2e2a71a4213074d6b80e9925686ac8
86138 <1695450299@qq.com>
2021-01-11 16:36:34 +0800

    阿树的图形界面

---
+++ app/src/main/java/com/xianyu/yixian_client/Model/PersonalInformation/History_Activity.java
@@ -0,0 +1,16 @@
+package com.xianyu.yixian_client.Model.PersonalInformation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.xianyu.yixian_client.R;
+
+public class History_Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_information_activity_);
+    }
+}
