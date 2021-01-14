b10b3b55f8838b2766cdcb98afdf4c1f447c899d
秦笑笑的本地用户 <2578908933@qq.com>
2021-01-14 17:57:09 +0800

    笑笑修改

--- app/src/main/java/com/xianyu/yixian_client/Main/MainActivity.java
+++ app/src/main/java/com/xianyu/yixian_client/Main/MainActivity.java
@@ -1,10 +1,13 @@
 package com.xianyu.yixian_client.Main;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,53 +36,30 @@ public class MainActivity extends AppCompatActivity {
         mainViewModel = new MainViewModel();
         Intent intentOne = new Intent(this, LoginService.class);
         startService(intentOne);
+        //视频初始化
+        VideoView videoView = findViewById(R.id.videoView_main);
+        videoView.setVideoPath(Uri.parse("android.resource://" + getPackageName() + "/raw/" + R.raw.cg_bg).toString());//路径
+        videoView.setOnPreparedListener(mp -> mp.setLooping(true));//循环
+        videoView.start();//开始
+
+        //置透明
+        View v = findViewById(R.id.GameType_button1);//找到你要设透明背景的layout 的id
+        View v1 = findViewById(R.id.GameType_button2);//找到你要设透明背景的layout 的id
+        View v2 = findViewById(R.id.button_enjoy);//找到你要设透明背景的layout 的id
+        View v3 = findViewById(R.id.button_official);//找到你要设透明背景的layout 的id
+        View v4 = findViewById(R.id.button_Shopping);//找到你要设透明背景的layout 的id
+        View v5 = findViewById(R.id.button_notice);//找到你要设透明背景的layout 的id
+        v.getBackground().setAlpha(10);//0~255透明度值
+        v1.getBackground().setAlpha(250);//0~255透明度值
+        v2.getBackground().setAlpha(20);//0~255透明度值
+        v3.getBackground().setAlpha(20);//0~255透明度值
+        v4.getBackground().setAlpha(20);//0~255透明度值
+        v5.getBackground().setAlpha(20);//0~255透明度值
 
         TextView text1 = binding.getRoot().findViewById(R.id.textView_experience);
         TextView text2 = binding.getRoot().findViewById(R.id.textView_username);
 
-        /*mainViewModel.message.observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
-                if(!text1.getText().toString().equals(s)){
-                    text1.setText(s);
-                }
-                if(!text2.getText().toString().equals(s)){
-                    text2.setText(s);
-                }
-            }
-        });
-        text1.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                mainViewModel.message.postValue(s.toString());
-                }
-        });
-        text2.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                mainViewModel.message.postValue(s.toString());
-            }
-        });*/
         text1.setText("我是复制体");
     }
 }
